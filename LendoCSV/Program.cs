e378a082794225d47cb5f8219c48d56c962d64a3
BRENO-MORAIS <brenofranciscomorais@gmail.com>
2020-07-06 22:14:50 -0400

    -Config-

---
+++ LendoCSV/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace LendoCSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Linhas = new List<string>();
+            using (StreamReader arquivo = new StreamReader(@"C:\_DEV\SistemaIntegracao\View\Txt\geo_bairro.csv")) ;
+            {
+                using (
+                StreamReader arquivo = File.OpenText(@"C:\_DEV\SistemaIntegracao\View\Txt\geo_bairro.csv"))
+                {
+                    string Linha = arquivo.ReadLine();
+                    while (true)
+                    {
+                        if (Linha != null)
+                        {
+                            Console.WriteLine(Linha);
+                            Linha = arquivo.ReadLine();
+                            Linhas.Add(Linha);
+                        }
+                    }
+                    foreach (var item in Linhas)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    Console.ReadKey();
+                }
+            }
+        }  } }
